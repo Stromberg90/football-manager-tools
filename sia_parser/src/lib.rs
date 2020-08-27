62d10117a3ec548c52feba2ca2fa6aa6a52c5c8c
Stromberg90 <andreas.stromberg90@gmail.com>
2020-08-27 02:35:24 +0200

    Some cleanup and initial support for sub meshes in the viewer.

--- sia_parser/src/lib.rs
+++ sia_parser/src/lib.rs
@@ -2,7 +2,7 @@ use byteorder::{LittleEndian, ReadBytesExt};
 use nalgebra::{Vector2, Vector3};
 use std::fs::File;
 use std::io::Seek;
-use std::io::{Read, Write};
+use std::io::{Read, SeekFrom, Write};
 use std::path::{Path, PathBuf};
 use std::str;
 
@@ -81,18 +81,10 @@ impl Mesh {
 
 #[derive(Debug)]
 pub struct Model {
-    pub unknown0: [u8; 12],
     pub who_knows: f32,
     pub objects_num: u32,
     pub object_id: u32,
     pub something_about_faces_or_vertices: u32,
-    pub unknown: [u8; 4],
-    pub unknown2: [u8; 8],
-    pub unknown3: [u8; 16],
-    pub unknown4: [u8; 24],
-    pub unknown4a: [u8; 24],
-    pub unknown4b: [u8; 16],
-    pub unknown5: u32,
     pub maybe_version: u32,
     pub name: String,
     pub bounding_box: BoundingBox,
@@ -106,18 +98,10 @@ pub struct Model {
 impl Model {
     fn new() -> Self {
         Model {
-            unknown0: [0u8; 12],
             who_knows: 0f32,
             objects_num: 0,
             object_id: 0,
             something_about_faces_or_vertices: 0,
-            unknown: [0u8; 4],
-            unknown2: [0u8; 8],
-            unknown3: [0u8; 16],
-            unknown4: [0u8; 24],
-            unknown4a: [0u8; 24],
-            unknown4b: [0u8; 16],
-            unknown5: 0,
             maybe_version: 0,
             name: String::new(),
             bounding_box: BoundingBox::new(),
@@ -141,8 +125,49 @@ fn read_string(file: &mut File) -> String {
     str::from_utf8(&string_buf).unwrap().to_owned()
 }
 
+fn read_bounding_box(file: &mut File) -> BoundingBox {
+    BoundingBox {
+        min_x: file.read_f32::<LittleEndian>().unwrap(),
+        min_y: file.read_f32::<LittleEndian>().unwrap(),
+        min_z: file.read_f32::<LittleEndian>().unwrap(),
+        max_x: file.read_f32::<LittleEndian>().unwrap(),
+        max_y: file.read_f32::<LittleEndian>().unwrap(),
+        max_z: file.read_f32::<LittleEndian>().unwrap(),
+    }
+}
+
+fn read_vector3(file: &mut File) -> Vector3<f32> {
+    let x = file.read_f32::<LittleEndian>().unwrap();
+    let y = file.read_f32::<LittleEndian>().unwrap();
+    let z = file.read_f32::<LittleEndian>().unwrap();
+    Vector3::new(x, y, z)
+}
+
+fn read_vector2(file: &mut File) -> Vector2<f32> {
+    let x = file.read_f32::<LittleEndian>().unwrap();
+    let y = file.read_f32::<LittleEndian>().unwrap();
+    Vector2::new(x, y)
+}
+
+fn read_triangle(file: &mut File) -> Triangle {
+    Triangle(
+        file.read_u16::<LittleEndian>().unwrap(),
+        file.read_u16::<LittleEndian>().unwrap(),
+        file.read_u16::<LittleEndian>().unwrap(),
+    )
+}
+
+trait Skip {
+    fn skip(&mut self, num_bytes: i64);
+}
+
+impl Skip for File {
+    fn skip(&mut self, num_bytes: i64) {
+        self.seek(SeekFrom::Current(num_bytes)).unwrap();
+    }
+}
+
 pub fn parse<P: AsRef<Path>>(filepath: P) -> Model {
-    dbg!(filepath.as_ref());
     let mut file = File::open(filepath).unwrap();
 
     let mut model = Model::new();
@@ -160,31 +185,22 @@ pub fn parse<P: AsRef<Path>>(filepath: P) -> Model {
 
     // So far these bytes have only been zero,
     // changing them did nothing
-    model.unknown0 = [0u8; 12];
-    file.read_exact(&mut model.unknown0).unwrap();
+    file.skip(12);
 
     // This might be some sort of scale, since it tends to resemble another bouding box value
     // changing it did nothing
     model.who_knows = file.read_f32::<LittleEndian>().unwrap();
 
-    model.bounding_box.min_x = file.read_f32::<LittleEndian>().unwrap();
-    model.bounding_box.min_y = file.read_f32::<LittleEndian>().unwrap();
-    model.bounding_box.min_z = file.read_f32::<LittleEndian>().unwrap();
-
-    model.bounding_box.max_x = file.read_f32::<LittleEndian>().unwrap();
-    model.bounding_box.max_y = file.read_f32::<LittleEndian>().unwrap();
-    model.bounding_box.max_z = file.read_f32::<LittleEndian>().unwrap();
+    model.bounding_box = read_bounding_box(&mut file);
 
     model.objects_num = file.read_u32::<LittleEndian>().unwrap();
-    dbg!(&model.objects_num);
 
     // So far has been 0's, when I changed it the mesh became invisible
-    model.unknown = [0u8; 4];
-    file.read_exact(&mut model.unknown).unwrap();
+    file.skip(4);
 
     model.num_vertices = file.read_u32::<LittleEndian>().unwrap();
     // Zero's, changing it made it invisible
-    model.unknown5 = file.read_u32::<LittleEndian>().unwrap();
+    file.skip(4);
 
     // This diveded by 3 gives the amount of faces, like another set of bytes later on
     // I'm wondering if this is the total amount of faces, and the other one is per mesh
@@ -193,21 +209,17 @@ pub fn parse<P: AsRef<Path>>(filepath: P) -> Model {
     // This needs to be moved into the mesh, then maybe when reading faces/vertices/materials one can match against it.
     model.object_id = file.read_u32::<LittleEndian>().unwrap();
 
-    dbg!(&model.object_id);
     // Changing these did nothing
-    model.unknown2 = [0u8; 8];
-    file.read_exact(&mut model.unknown2).unwrap();
-    dbg!(model.unknown2);
+    file.skip(8);
+
+    file.skip(((model.objects_num - 1) * 28).into());
 
     model.num_meshes = file.read_u32::<LittleEndian>().unwrap();
-    dbg!(file.seek(std::io::SeekFrom::Current(0)));
-    dbg!(model.num_meshes);
 
     // Changing these did nothing
-    model.unknown3 = [0u8; 16];
-    file.read_exact(&mut model.unknown3).unwrap();
+    file.skip(16);
 
-    for _ in 0..model.num_meshes {
+    for i in 0..model.num_meshes {
         let mut mesh = Mesh::new();
         let material_kind = read_string(&mut file);
         mesh.materials_num = file.read_u8().unwrap();
@@ -217,74 +229,59 @@ pub fn parse<P: AsRef<Path>>(filepath: P) -> Model {
             material.name = read_string(&mut file);
             material.textures_num = file.read_u8().unwrap();
             for _ in 0..material.textures_num {
-                let uknown = file.read_u8().unwrap(); //Maybe a way to identify which texture it is, like a id
+                let texture_id = file.read_u8().unwrap(); //Maybe a way to identify which texture it is, like a id
                 let texture = Texture {
                     name: read_string(&mut file),
-                    id: uknown,
+                    id: texture_id,
                 };
                 material.textures.push(texture);
             }
             mesh.materials.push(material);
         }
         model.meshes.push(mesh);
+        if i != model.num_meshes - 1 {
+            file.skip(80);
+        }
     }
-
-    model.unknown4 = [0u8; 24];
-    file.read_exact(&mut model.unknown4).unwrap();
-    model.unknown4a = [0u8; 24];
-    file.read_exact(&mut model.unknown4a).unwrap();
-    model.unknown4b = [0u8; 16];
-    file.read_exact(&mut model.unknown4b).unwrap();
+    file.skip(64);
 
     // Maybe this is for this mesh, and the earlier one is for the entire file.
     let local_num_vertecies = file.read_u32::<LittleEndian>();
 
-    let maybe_a_identifer_num = file.read_u32::<LittleEndian>();
+    let _maybe_a_identifer_num = file.read_u32::<LittleEndian>();
 
-    for _ in 0..model.num_vertices {
-        let pos_x = file.read_f32::<LittleEndian>().unwrap();
-        let pos_y = file.read_f32::<LittleEndian>().unwrap();
-        let pos_z = file.read_f32::<LittleEndian>().unwrap();
+    for _ in 0..local_num_vertecies.unwrap() {
+        // for _ in 0..model.num_vertices {
+        let pos = read_vector3(&mut file);
 
         // Think these are normals, when plotted out as vertices, they make a sphere.
         // Which makes sense if it's normals
-        let normal_x = file.read_f32::<LittleEndian>().unwrap();
-        let normal_y = file.read_f32::<LittleEndian>().unwrap();
-        let normal_z = file.read_f32::<LittleEndian>().unwrap();
-        println!("Normals");
-        println!("\t{}", normal_x);
-        println!("\t{}", normal_y);
-        println!("\t{}", normal_z);
+        // Actually, I'm second guessing myself
+        let normal = read_vector3(&mut file);
 
-        let uv_x = file.read_f32::<LittleEndian>().unwrap();
-        let uv_y = file.read_f32::<LittleEndian>().unwrap();
+        let uv = read_vector2(&mut file);
 
-        println!("Unknowns");
+        // println!("Unknowns");
         // Thinking these are tangents or binormals, last one is always 1 or -1
-        println!("\t{}", file.read_f32::<LittleEndian>().unwrap());
-        println!("\t{}", file.read_f32::<LittleEndian>().unwrap());
-        println!("\t{}", file.read_f32::<LittleEndian>().unwrap());
-        println!("\t{}", file.read_f32::<LittleEndian>().unwrap());
+        file.skip(16);
 
         model.vertices.push(Vertex {
-            position: Vector3::new(pos_x, pos_y, pos_z),
-            uv: Vector2::new(uv_x, uv_y),
-            normals: Vector3::new(normal_x, normal_y, normal_z),
+            position: pos,
+            uv,
+            normals: normal,
         })
     }
 
     let number_of_triangles = file.read_u32::<LittleEndian>().unwrap() / 3;
+
     for _ in 0..number_of_triangles {
-        let first = file.read_u16::<LittleEndian>().unwrap();
-        let second = file.read_u16::<LittleEndian>().unwrap();
-        let third = file.read_u16::<LittleEndian>().unwrap();
-        model.triangles.push(Triangle(first, second, third));
+        model.triangles.push(read_triangle(&mut file));
     }
-    let mut padding = [0u8; 13];
-    file.read_exact(&mut padding).unwrap();
+    file.skip(13);
 
     let mut end_file_tag = [0u8; 4];
     file.read_exact(&mut end_file_tag).unwrap();
+    return model;
     let end_file_tag = str::from_utf8(&end_file_tag).unwrap();
     if end_file_tag != "EHSM" {
         panic!("Expexted EHSM, but found {}", end_file_tag);
